cd875257998bcd14990869e08d16aadbe422b535
Travis Hedrick <iCommStudios@Traviss-MacBook-Pro.local>
2014-02-11 16:50:39 -0500

    Pushed header re-work to live site

--- wp-content/themes/GroupBuyingSite-gbs-prime-child-theme-890d949/inc/navigation2.php
+++ wp-content/themes/GroupBuyingSite-gbs-prime-child-theme-890d949/inc/navigation2.php
@@ -8,12 +8,27 @@
 
 <div id="header_wrap" class="prime boxed_prime clearfix">
 
-	<div id="lst_header" class="container clearfix">
+	<div id="header" class="container clearfix">
 
-			<a id="lst_logo" href="<?php echo site_url() ?>"><img src="<?php gb_header_logo() ?>"></a>
+		<h1 id="lst_logo" class="clearfix">
+			<a href="<?php echo site_url() ?>"><img src="<?php gb_header_logo() ?>"></a>
+		</h1>
 
 		<div class="lst_header_meta">
-			<div id="lst_login_form">
+			<?php $locations = gb_get_locations();
+			if ( !empty( $locations ) && !is_wp_error( $locations ) ) : ?>
+				<div id="location">
+					<div class="header-locations-drop-link gb_ff">
+						<span class="current_location"><?php gb_current_location_extended(); ?></span>
+
+						<div id="locations_header_wrap" class="clearfix cloak header_color font_small">
+							<?php gb_list_locations(); ?>
+							</div><!-- #locations_header_wrap. -->
+						</div>
+				</div>
+			<?php endif; ?>
+
+			<div id="login_form">
 				<div id="login_wrap" class="gb_ff font_small clearfix">
 					<?php if ( !is_user_logged_in() ): ?>
 						<a href="<?php echo wp_login_url(); ?>" class="head-login-drop-link"><?php gb_e( 'Login' ) ?></a>
@@ -26,29 +41,16 @@
 								<span class="gravatar"><?php gb_gravatar() ?></span>
 								<?php gb_e( 'Hi,' ) ?> <?php gb_name() ?></a>
 							</span>
-			<span><a href="<?php gb_cart_url() ?>"><?php gb_e( 'Cart' ) ?></span> | 
-			<span class="header_cart"><a href="<?php gbs_account_link() ?>" class="name" title="<?php gb_e( 'Your Account' ) ?>"><?php gb_e( 'My Account' ) ?></a></span> | 
-			<?php gb_logout_url(); ?>
+							<span class="header_cart"><a href="<?php gbs_account_link() ?>" class="name" title="<?php gb_e( 'Your Account' ) ?>"><?php gb_e( 'My Account' ) ?></a></span> | <?php gb_logout_url(); ?>
 						</div>
 					<?php endif ?>
-					<?php $locations = gb_get_locations();
-							if ( !empty( $locations ) && !is_wp_error( $locations ) ) : ?>
-								<div id="location">
-									<div class="header-locations-drop-link gb_ff">
-										<span class="current_location"><?php gb_current_location_extended(); ?></span>
 
-										<div id="locations_header_wrap" class="clearfix cloak header_color">
-											<?php gb_list_locations(); ?>
-											</div><!-- #locations_header_wrap. -->
-										</div>
-								</div>
-							<?php endif; ?>
 				</div><!-- #login_wrap -->
 			</div>
 
 		</div>
 
-		<div class="lst-header-search" tabindex="2">
+		<div class="lst-header-search">
 				<?php get_search_form(); ?> 
 			</div>
 
