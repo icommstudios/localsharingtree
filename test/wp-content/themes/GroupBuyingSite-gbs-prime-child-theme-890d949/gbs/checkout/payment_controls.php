6d9c6d58274343b62bae91f1e9b4b84b91d784ac
Travis Hedrick <iCommStudios@Traviss-MacBook-Pro.local>
2014-03-26 14:04:32 -0400

    Edit Checkout page - make full page

---
+++ test/wp-content/themes/GroupBuyingSite-gbs-prime-child-theme-890d949/gbs/checkout/payment_controls.php
@@ -0,0 +1,7 @@
+<div class="alert danger text-center"><h4>Note: All fields are required to Checkout.</h4></div>
+
+<div class="checkout-controls clearfix">
+	<?php foreach ( $form_controls as $control ): ?>
+		<?php echo $control; ?>
+	<?php endforeach; ?>
+</div>
