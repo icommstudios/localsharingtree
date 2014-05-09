ed5c6724cef0dab3af8292621ce0ecb89ae6bc60
Travis Hedrick <iCommStudios@Traviss-MacBook-Pro.local>
2014-05-09 12:47:43 -0400

    Add Paypal disclaimer

--- wp-content/themes/GroupBuyingSite-gbs-prime-child-theme-890d949/gbs/checkout/payment_controls.php
+++ wp-content/themes/GroupBuyingSite-gbs-prime-child-theme-890d949/gbs/checkout/payment_controls.php
@@ -1,7 +1,8 @@
 <div class="alert danger text-center"><h4>Note: All fields are required to Checkout.</h4></div>
 
-<div class="checkout-controls clearfix">
+<div class="checkout-controls text-center clearfix">
 	<?php foreach ( $form_controls as $control ): ?>
 		<?php echo $control; ?>
 	<?php endforeach; ?>
+	<p>All transactions are processed through <strong>Paypal's Secure Server</strong>. However, a Paypal account is NOT REQUIRED.<br>You will have the option to pay with a credit card OR a PayPal account.</p>
 </div>
